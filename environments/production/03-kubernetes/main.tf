e332a57ca3e9ddb63de949af6585a15aaf2bc87e
Jorijn Schrijvershof <jorijn@jorijn.com>
2022-12-18 23:34:06 +0100

    change the autoscaling pool to allow more nodes

--- environments/production/03-kubernetes/main.tf
+++ environments/production/03-kubernetes/main.tf
@@ -10,13 +10,13 @@ module "kubernetes" {
   generic_pool_name      = "pool-generic-3"
   generic_pool_size      = "s-4vcpu-8gb-amd"
   generic_pool_min_nodes = 2
-  generic_pool_max_nodes = 8
+  generic_pool_max_nodes = 12
 
   # Node pool - sidekiq
   sidekiq_pool_name      = "pool-sidekiq-3"
   sidekiq_pool_size      = "s-4vcpu-8gb-amd"
   sidekiq_pool_min_nodes = 1
-  sidekiq_pool_max_nodes = 4
+  sidekiq_pool_max_nodes = 6
 
   # Networking
   vpc_name = "ams3-vpc-toot-community-01"
