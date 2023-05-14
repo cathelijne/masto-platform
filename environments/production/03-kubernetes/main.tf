85e149be0679e1e55c1ee22dade5d097926de6c1
Jorijn Schrijvershof <jorijn@jorijn.com>
2023-05-14 18:29:10 +0200

    revert to previous infra setup, outage of may 5th didnt have to do with capacity

--- environments/production/03-kubernetes/main.tf
+++ environments/production/03-kubernetes/main.tf
@@ -8,10 +8,10 @@ module "kubernetes" {
   high_availability    = true
 
   # Node pool - generic
-  generic_pool_name      = "pool-generic-3"
-  generic_pool_size      = "g-4vcpu-16gb"
-  generic_pool_min_nodes = 2
-  generic_pool_max_nodes = 10
+  generic_pool_name      = "pool-generic-2"
+  generic_pool_size      = "s-4vcpu-8gb-amd"
+  generic_pool_min_nodes = 3
+  generic_pool_max_nodes = 8
 
   # Node pool - sidekiq
   sidekiq_pool_name      = "pool-sidekiq-3"
@@ -22,8 +22,8 @@ module "kubernetes" {
   # Node pool - db
   db_pool_name      = "pool-db-2"
   db_pool_size      = "g-4vcpu-16gb"
-  db_pool_min_nodes = 2
-  db_pool_max_nodes = 4
+  db_pool_min_nodes = 1
+  db_pool_max_nodes = 2
 
   # Networking
   vpc_name = "ams3-vpc-toot-community-01"
